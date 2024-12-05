b0169ccab5f5c403945c006a397462baf39ef091
Sumit <sumitdhiman@outlook.in>
2024-12-05 17:02:11 +0530

    Update background.js
    Removed unnecessary code.

--- background.js
+++ background.js
@@ -4,14 +4,3 @@ chrome.commands.onCommand.addListener((command) => {
 
   }
 });
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
-        chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            files: ['content.js']
-        })
-          .then(() => console.log('injected script into: ' + tab.url))
-          .catch(err => console.log(err));
-    }
-});
