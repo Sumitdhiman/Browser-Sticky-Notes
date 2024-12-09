b4a4b6e7a057224c4c1d604c7b067b38018b4817
Sumit <sumitdhiman@outlook.in>
2024-12-09 18:16:27 +0530

    Delete content.js
    Irrelevant content removed.

--- content.js
+++
@@ -1,8 +0,0 @@
-if (!window.hasBeforeUnloadListener) {
-    window.hasBeforeUnloadListener = true;
-    window.addEventListener('beforeunload', function (e) {
-        e.preventDefault();
-        e.returnValue = 'Are you sure you want to close?';
-        return e.returnValue;
-    }, true);
-}
