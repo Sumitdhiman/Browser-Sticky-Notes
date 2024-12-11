dc1a702f51d256ee1dcff815545ceab6e8e2639f
Sumit <sumitdhiman@outlook.in>
2024-12-11 20:50:49 +0530

    Fixed bugs
    Updated shortcut and fixed bugs

--- background.js
+++ background.js
@@ -1,23 +1,11 @@
 chrome.commands.onCommand.addListener((command) => {
-    if (command === 'open_popup') {
-        chrome.action.openPopup();
-    }
-});
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
-        chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            files: ['content.js']
-        })
-          .then(() => console.log('injected script into: ' + tab.url))
-          .catch(err => console.log(err));
-
-    }
+  if (command === 'open_popup') {
+      chrome.action.openPopup();
+  }
 });
 chrome.runtime.onInstalled.addListener((details) => {
-    if (details.reason === 'install') {
-      // This is the first install
-      chrome.tabs.create({ url: 'options.html' });
-    }
-  });
-  
+  if (details.reason === 'install') {
+    // This is the first install
+    chrome.tabs.create({ url: 'options.html' });
+  }
+});
